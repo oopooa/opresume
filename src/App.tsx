42db27ff22a36e56351e7428a012dad7bc33daa4
opoa <oopooa@outlook.com>
2026-02-20 11:10:43 +0800

    feat: init project with react, vite, and tailwind css setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">Hello, opresume!</h1>
+    </div>
+  );
+}
+
+export default App;
