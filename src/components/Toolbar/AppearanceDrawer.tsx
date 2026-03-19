9bc6e9df846505bb1cb7015d374ef60965b9eea9
opoa <oopooa@outlook.com>
2026-03-19 22:26:52 +0800

    fix: prevent auto-focus on template dialog open

--- src/components/Toolbar/AppearanceDrawer.tsx
+++ src/components/Toolbar/AppearanceDrawer.tsx
@@ -261,7 +261,7 @@ export function AppearanceDrawer() {
 
       {/* 模板选择 Dialog */}
       <Dialog open={templateDialogOpen} onOpenChange={setTemplateDialogOpen}>
-        <DialogContent className="max-w-5xl max-h-[85vh] flex flex-col">
+        <DialogContent className="max-w-5xl max-h-[85vh] flex flex-col" onOpenAutoFocus={(e) => e.preventDefault()}>
           <DialogHeader>
             <DialogTitle>{t('toolbar.template')}</DialogTitle>
             <DialogDescription className="sr-only">{t('toolbar.templateDesc')}</DialogDescription>
